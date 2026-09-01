61db357edfdeba8a26f227f18b9e13695b58ab57
arturdacyk-star <artur.dacyk@gmail.com>
2026-09-01 21:41:04 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    tiles: those are live data and should always try the network first;
    the browser's normal HTTP cache already helps with repeat tile requests.
 */
-const CACHE_NAME = 'tripdot_v1';
+const CACHE_NAME = 'tripdot_v2';
 
 const APP_SHELL = [
   './',
